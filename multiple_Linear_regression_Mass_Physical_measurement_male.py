74a29e39932c62afc4dad861329a385d1e608be1
Arunabha Gupta <31182790+arunabhagupta@users.noreply.github.com>
2018-04-27 13:13:10 +0530

    Update multiple_Linear_regression_Mass_Physical_measurement_male.py

--- multiple_Linear_regression_Mass_Physical_measurement_male.py
+++ multiple_Linear_regression_Mass_Physical_measurement_male.py
@@ -28,3 +28,20 @@ regressor_OLS.summary()
 X_opt = X[:,[0,1,6,7,9]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
+
+import statsmodels.formula.api as sm
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
+X_opt = X[:, [0,1,2,3,4,5,6,7,8,9,10]]
+X_Modeled = backwardElimination(X_opt, SL)
